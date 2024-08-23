9b2e00c42858285c970f341f61744c76e482177b
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-08-23 15:52:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-qimport re
+import re
 import os
 from os import environ
 from Script import script
@@ -59,7 +59,7 @@ SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002092666907'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1002044964710')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002037836704'))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002170760514'))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', ''))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '5000')

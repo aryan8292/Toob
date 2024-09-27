e1499c7d128d54f7421c50c74a0adafdbab3666f
maan12305 <167197439+maan12305@users.noreply.github.com>
2024-09-27 13:28:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,8 +44,8 @@ SHORTENER_API2 = environ.get("SHORTENER_API2", "363302bd475d834760cf6b6870256730
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'omegalinks.in')
 SHORTENER_API3 = environ.get("SHORTENER_API3", "omegalinks.in")
 SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", '363302bd475d834760cf6b687025673068fefee5')
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "3600"))
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "3600"))
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "28800"))
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "28800"))
 
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi"]
 QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
